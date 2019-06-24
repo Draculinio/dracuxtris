f7f27ea13c8c9e66c4590af054fba56c4fef3a31
Pablo Soifer <stackpointerex@gmail.com>
2019-06-23 21:33:05 -0300

    Arreglando código

--- cosa.py
+++ cosa.py
@@ -14,41 +14,51 @@ def presentacion(screen):
     screen.clear()
 
 def tablero(screen):
-    screen.addstr(25,10,"_______")
+    screen.addstr(25,10,"______________")
     for i in range(5,25):
         screen.addstr(i,9,"|")
-
-screen = curses.initscr()
-curses.noecho()
-curses.curs_set(0)
-presentacion(screen)
-tablero(screen)
+        screen.addstr(i,25,"|")
+    screen.refresh()    
 
 
-for i in range(0,25):
-    screen.addstr(i,10,'**')
-    screen.addstr(i+1,10,'**')
-    screen.refresh()
-    time.sleep(0.1)
-    if i<24:
-        screen.addstr(i,10,'  ')
-        screen.addstr(i+1,10,'  ')
+def juego(screen):
+    for i in range(0,25):
+        screen.addstr(i,10,'**')
+        screen.addstr(i+1,10,'**')
         screen.refresh()
         time.sleep(0.1)
+        if i<24:
+            screen.addstr(i,10,'  ')
+            screen.addstr(i+1,10,'  ')
+            screen.refresh()
+            time.sleep(0.1)
     
-    screen.refresh()
-    time.sleep(0.1)
-for i in range(0,24):
-    screen.addstr(i,12,'*')
-    screen.addstr(i+1,12,'**')
-    screen.addstr(i+2,12,' *')
-    screen.refresh()
-    time.sleep(0.1)
-    screen.addstr(i,12,' ')
-    screen.addstr(i+1,12,'  ')
-    screen.addstr(i+2,12,' ')
-    screen.refresh()
-    time.sleep(0.1)
+        screen.refresh()
+        time.sleep(0.1)
+    for i in range(0,24):
+        screen.addstr(i,12,'*')
+        screen.addstr(i+1,12,'**')
+        screen.addstr(i+2,12,' *')
+        screen.refresh()
+        time.sleep(0.1)
+        screen.addstr(i,12,' ')
+        screen.addstr(i+1,12,'  ')
+        screen.addstr(i+2,12,' ')
+        screen.refresh()
+        time.sleep(0.1)
 
-curses.endwin()
+#MAIN
+def main():
+    screen = curses.initscr()
+    curses.noecho()
+    curses.curs_set(0)
+    screen.border(2)
+    #win = curses.newwin(0,0,24,70)
+    presentacion(screen)
+    tablero(screen)
+    juego(screen)
+    time.sleep(1)
+    curses.endwin()
 
+if __name__=='__main__':
+    main()
